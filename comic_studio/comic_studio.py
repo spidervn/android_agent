f0be8600de42134ca486144f5b8ab21af826e57c
thaibinhduong <a@vnn.vn>
2017-06-03 09:41:08 +0700

    add comic studio

---
+++ comic_studio/comic_studio.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+
+
+def ComicStudioMain:
+
+    def __init__(self):
+        self.count = 0
+
+        self.dict_knowledge = {}    # Dictionary of task 
+        self.core_task = {}         # Core of Tasking
+        self.combine_task = {}      # Combination of tasks
+
+
+    def install_task():             # Ability to Install new task
+        return
+
+    def work():
+        return 
+
+def DoDomesticTask:
+
+    def __init__(self):
+        self.count = 1
+
+def AutoAsk:
+    
+    def __init__(self):
+        print "Done"
+
+    def __init__(self):
+        print "Done"
