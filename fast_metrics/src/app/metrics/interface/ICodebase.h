56b9b4426a8c23dcee0677f47d096addbfc07de8
ducvd <truyenspidermanvnl@gmail.com>
2018-01-05 18:14:26 +0700

    New interface foce codebase

---
+++ fast_metrics/src/app/metrics/interface/ICodebase.h
@@ -0,0 +1,22 @@
+/*
+ * ICodebase.h
+ *
+ *  Created on: Jan 5, 2018
+ *      Author: ducvd
+ */
+
+#ifndef APP_METRICS_INTERFACE_ICODEBASE_H_
+#define APP_METRICS_INTERFACE_ICODEBASE_H_
+
+
+class ICodebase
+{
+public:
+	virtual ~ICodebase() {}
+	virtual long line_OfCode() = 0;
+	virtual long count_Classes() = 0;
+	virtual long count_Methods() = 0;	
+};
+
+
+#endif /* APP_METRICS_INTERFACE_ICODEBASE_H_ */
