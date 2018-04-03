f2a9fce69d221af9df8729bc5cdccaadd21a9407
ducvd <truyenspidermanvnl@gmail.com>
2018-04-03 16:22:03 +0700

    New methodologies

--- fast_metrics/src/app/estimation/interface/IApproach.h
+++ fast_metrics/src/app/estimation/interface/IApproach.h
@@ -8,6 +8,14 @@
 #ifndef APP_ESTIMATION_INTERFACE_IAPPROACH_H_
 #define APP_ESTIMATION_INTERFACE_IAPPROACH_H_
 
+#define APPROACH_LINE_OF_CODE 1	// Just count line of code
+#define APPROACH_WELL_KNOWN 2	// Every task must well known; Any non-wellknown task is not acceptable
+#define APPROACH_SAFETY	3		// Safety <=> Want to know everything 
+								//	Including 
+								// 		Well-know tasks| Non-Well Known | Unknown | Unknown of Unknown
+#define APPROACH_VERY_FIRST	4	// Approach from the very first idea
+#define APPROACH_WORKER_VILLAGER 5	// Most dummy method (Linear)
+									
 class IApproach
 {
 public:
